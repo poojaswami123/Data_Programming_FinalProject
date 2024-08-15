7e3d35335772a61bb57fb85701f375d53d2e43f5
poojaswami123 <200590358@student.georgianc.on.ca>
2024-08-15 19:47:34 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+﻿# C:\Users\swami\source\repos\XYZ_Corporation\XYZ_Corporation\config.py
+class Config:
+    # Your configuration settings here
+    DATABASE_URI = 'sqlite:///mydatabase.db'
+    SECRET_KEY = 'your-secret-key'
+    # Other configuration settings
